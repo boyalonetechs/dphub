8bdc7535695c86a5667c21e5d1c80d68f2a803a1
Boy Alone <boyalonetechs@gmail.com>
2026-05-20 09:14:15 +0000

    chore: rename project to dphub and update contact information in footer

--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 
-
 const FacebookIcon = ({ className }: { className?: string }) => (
   <svg className={className} fill="currentColor" viewBox="0 0 24 24">
     <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" />
@@ -50,8 +49,14 @@ export default function Footer() {
                 Let&apos;s build something great together
               </h3>
               <p className="text-gray-400 text-sm">
-                Subscribe to our newsletter to receive the latest updates, news,
-                and insights.
+                <span className=" block text-white mb-2 font-bold">
+                  Email: Dphubbyleadpath@gmail.com
+                </span>
+                <span className=" block text-white mb-2 font-semibold">
+                  Phone:+2349012601449
+                </span>
+                Address: No 5 Umuola Road Opposite Evergreen Hotel, Ogborhill
+                Aba, Abia State
               </p>
             </div>
           </div>
